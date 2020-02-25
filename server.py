b55058f9e42cc06576d6c5370cd86cd9a1c671df
Alexies <im@alexiesracca.com>
2020-02-26 04:38:57 +0800

    Added CORS config

--- server.py
+++ server.py
@@ -2,11 +2,13 @@
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 from dataclasses import dataclass
 import nfc_service
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 @dataclass
